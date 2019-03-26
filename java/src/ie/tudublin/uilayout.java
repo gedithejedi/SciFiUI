8759ce8ff1dca86defd1f470d262c2e66200e69f
Gediminas <c17323756@mydit.ie>
2019-03-26 01:33:05 +0000

    finished drawing the top part

--- java/src/ie/tudublin/uilayout.java
+++ java/src/ie/tudublin/uilayout.java
@@ -15,7 +15,19 @@ public class UiLayout
 
     public void render()
     {
-		ui.rect(50, 10, 100, 200); // tlx, tly, w, h
-			
-	}
+			//Drawing the top left part
+			ui.stroke(255,0,0);
+			ui.line(0, 133, 190, 108);
+			ui.line(190, 108, 240, 40);
+			ui.line(240, 40, 205, 0);
+			//drawing the top middle part
+			ui.line(175, 0, 230, 65);
+			ui.line(230, 65, 510, 80);
+			ui.line(510, 80, 790, 65);
+			ui.line(790, 65, 850, 0);
+			//Draring the top right part
+			ui.line(820, 0, 785, 40);
+			ui.line(785, 40, 840, 108);
+			ui.line(840, 108, 1025, 133);
+		}
 }
